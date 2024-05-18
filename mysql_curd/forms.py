25ba0d83b677e4f00b5afcda717a8daa82a54a20
MahendraN <mahendraramalingam8056@gmail.com>
2024-05-18 05:32:01 +0530

    Update

---
+++ mysql_curd/forms.py
@@ -0,0 +1,7 @@
+from django import forms               # here create new file forms.py for creating tables in easy method
+from .models import RegisterForm       # here import from models.py file
+
+class MyRegisterForm(forms.ModelForm):
+    class Meta:
+        model = RegisterForm
+        fields = ["name", "age", "address", "contacts", "email"]
