8106310929bc1cb595234ee74fbb5097c4451387
MahendraN <mahendraramalingam8056@gmail.com>
2024-05-24 19:32:29 +0530

    Update

--- mysql_proj/settings.py
+++ mysql_proj/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'mysql_curd',
     'crispy_forms',                 # Here include crispy file
+    'crispy_bootstrap4',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
