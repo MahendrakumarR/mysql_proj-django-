a5516beb62d9284a5c6cf07dc794cdb12d8a9954
MahendraN <mahendraramalingam8056@gmail.com>
2024-05-27 19:58:53 +0530

    Update

--- mysql_curd/views.py
+++ mysql_curd/views.py
@@ -1,10 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import MyRegisterForm
+from django.contrib import messages            # here using messages for below declaration
 
 def home(request):
     return render(request, "home.html")
 
 def insert(request):
-    form = MyRegisterForm()
+    if request.method == 'POST':
+        form = MyRegisterForm(request.POST)
+        if form.is_valid():
+            try:
+                form.save()
+                messages.success(request,"Registration Successfully Completed")
+                return redirect("home")        # Here using home name from urls.py
+            except:
+                pass
+    else:
+        form = MyRegisterForm()
     return render(request, "register.html", {'form':form})
 # Create your views here.
